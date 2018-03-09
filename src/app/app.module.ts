18fef89c2486dc07dd85cc030c4dc9c39d544928
Pravin Pawar <Pravin.Pawar@INFOVISIONLABS.COM>
2018-03-09 14:00:37 +0530

    primeng changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Button, ButtonDirective, ButtonModule } from 'primeng/button'
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Button,
+    ButtonDirective
   ],
   imports: [
     BrowserModule
