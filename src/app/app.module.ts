62969285fde4535567ee7bcfe4ebb9d279d87ed1
Pravin Pawar <Pravin.Pawar@INFOVISIONLABS.COM>
2018-03-09 15:41:30 +0530

    load dynamic component based on type

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Button, ButtonDirective, ButtonModule } from 'primeng/button'
 
+import { DynamicModule } from 'ng-dynamic-component';
+
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { AnotherChildComponent } from './another-child/another-child.component';
+import { ListComponent } from './list/list.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    ChildComponent,
+    AnotherChildComponent,
     Button,
-    ButtonDirective
+    ButtonDirective,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DynamicModule.withComponents([ChildComponent, AnotherChildComponent])
   ],
   providers: [],
   bootstrap: [AppComponent]
